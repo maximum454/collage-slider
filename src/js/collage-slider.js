86369307cc4103ad0082cf9792ab43547e9d6ae5
maxim <maximum454@gmail.com>
2021-04-20 12:27:09 +0600

    fix

--- src/js/collage-slider.js
+++ src/js/collage-slider.js
@@ -8,43 +8,34 @@ $(function () {
         let rand = min + Math.random() * (max + 1 - min);
         return Math.floor(rand);
     }
-
-
-
     function sliderCollage(){
-        let content = document.getElementsByClassName('collage__img');
+        let elemCollage = document.querySelectorAll('.collage__img');
         const arrTop = [100,0,55,-10,10,105,0,95,10,60,-10];
-        const arrWidth = [80,140,140,100,120,100,120,140,100,120,100];
-
-        for(var i = 0; i < content.length; i++) {
-            content[i].style.zIndex = randomInteger(1,3);
-            content[i].style.marginTop = arrTop[i]+'px';
-            content[i].style.width = arrWidth[i]+'px';
-            content[i].style.height = arrWidth[i]+'px';
+        const arrSize = [80,140,140,100,120,100,120,140,100,120,100];
+        let j = 0;
+        for(let i = 0; i < elemCollage.length; i++) {
+            if(j == 11) j = 0
+            elemCollage[i].style.zIndex = randomInteger(1,3);
+            elemCollage[i].style.marginTop = arrTop[j]+'px';
+            elemCollage[i].style.width = arrSize[j]+'px';
+            elemCollage[i].style.height = arrSize[j]+'px';
+            j++;
         }
     }
 
     sliderCollage();
 
     $(function (){
-        $('.responsive').slick({
+        $('.js-collage-slider').slick({
             dots: false,
             infinite: false,
             speed: 300,
             slidesToShow: 1,
             slidesToScroll: 1,
             adaptiveHeight: true,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
-                    }
-                }
-            ]
+        });
+        $('.js-collage-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+            sliderCollage();
         });
     })
 
