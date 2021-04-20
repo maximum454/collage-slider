3a180e68b0a0206a6b9afb12e9bd08f91db45cc5
maxim <maximum454@gmail.com>
2021-04-20 11:06:24 +0600

    fyncybox и наведение

--- src/js/collage-slider.js
+++ src/js/collage-slider.js
@@ -9,21 +9,22 @@ $(function () {
         return Math.floor(rand);
     }
 
-    var content = document.getElementsByClassName('collage__img');
-    var leftStart = 0;
-    var topStart = 0;
-    var arrTop = [100,0,55,-10,10,105,0,95,10,60,-10]
 
-    for(var i = 0; i < content.length; i++)
-    {
-        var elemWidth = content[i].offsetWidth;
 
-        var elemLeft = elemWidth;
-        content[i].style.zIndex = randomInteger(1,3);
+    function sliderCollage(){
+        let content = document.getElementsByClassName('collage__img');
+        const arrTop = [100,0,55,-10,10,105,0,95,10,60,-10];
+        const arrWidth = [80,140,140,100,120,100,120,140,100,120,100];
 
-        content[i].style.top = arrTop[i]+'px';
-
+        for(var i = 0; i < content.length; i++) {
+            content[i].style.zIndex = randomInteger(1,3);
+            content[i].style.marginTop = arrTop[i]+'px';
+            content[i].style.width = arrWidth[i]+'px';
+            content[i].style.height = arrWidth[i]+'px';
+        }
     }
+
+    sliderCollage();
 
     $(function (){
         $('.responsive').slick({
